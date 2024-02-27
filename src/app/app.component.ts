0cab0ad3faed681dcbe66968d50e6551efbb333d
obisopulu <obisopulu@gmail.com>
2024-02-27 22:47:10 +0100

    adding an data model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'wishlist';
+  items = [
+    new WishItem('New Phone', true),
+    new WishItem('New Laptop', true),
+    new WishItem('New Car', true),
+    new WishItem('New House', true),
+    new WishItem('New Job', true),
+    new WishItem('New Life', true)
+  ]
 }
